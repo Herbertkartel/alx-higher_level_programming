73b5b066207aef431a35e52abec05d21437e90b7
Herbertkartel <herbzkartel@gmail.com>
2022-12-21 12:48:57 -0800

    Task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -2,11 +2,12 @@
 from __future__ import print_function
 import sys
 
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return none
+        return None
     else:
         return res
